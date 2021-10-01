928b4b4b4b4a89f9a89d6469e2261092936226ad
Gkhnerylcn <86232454+Gkhnerylcn@users.noreply.github.com>
2021-10-01 14:25:10 +0300

    Urunler Web Service
    Veri tabanımdaki Ürünler tablosunun Web Service ile sunulması.

---
+++ WindowsFormsApp44/WebApplication1/UrunDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class UrunDTO
+    {
+        public int Urunid { get; set; }
+        public string UrunAd { get; set; }
+        public Nullable<short> UrunKategori { get; set; }
+        public Nullable<decimal> UrunFiyat { get; set; }
+        public string Marka { get; set; }
+        public Nullable<byte> Stok { get; set; }
+
+    }
+}
---
+++ WindowsFormsApp44/WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    using Models;
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        MvcDbStokEntities1 db = new MvcDbStokEntities1();
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<UrunDTO> UrunlerListesi()
+        {
+
+            var degerler = db.Ürünler.Select(x=>new UrunDTO {Urunid=x.Urunid,UrunAd=x.UrunAd,UrunKategori=x.UrunKategori,UrunFiyat=x.UrunFiyat,Marka=x.Marka,Stok=x.Stok }).ToList();
+            db.SaveChanges();
+            return (degerler);
+
+
+
+        }
+    }
+}
